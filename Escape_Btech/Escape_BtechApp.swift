512225ef3b20b085c9207fcab7425fe0477affec
1N4 <sample@bths.edu>
2025-05-20 08:59:05 -0400

    initial commit

---
+++ Escape_Btech/Escape_BtechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Escape_BtechApp.swift
+//  Escape_Btech
+//
+//  Created by Student on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Escape_BtechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
