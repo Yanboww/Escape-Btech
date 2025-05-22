b4d1fd65bca8b9d6206eb8b2fe6a0bd478fd5fba
DashielL23 <144473374+DashielL23@users.noreply.github.com>
2025-05-22 09:27:29 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Escape_Btech
+//
+//  Created by Student on 5/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrashGame/TrashGameModel.swift
@@ -0,0 +1,8 @@
+//
+//  TrashGameModel.swift
+//  Escape_Btech
+//
+//  Created by Student on 5/22/25.
+//
+
+import Foundation
