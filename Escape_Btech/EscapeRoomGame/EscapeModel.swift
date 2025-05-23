47e32e8ef208981204f9f975d9f262ceb3bad293
null <null>
2025-05-23 09:31:09 -0400

    Started on escape room model

---
+++ Escape_Btech/EscapeRoomGame/EscapeModel.swift
@@ -0,0 +1,15 @@
+//
+//  EscapeModel.swift
+//  Escape_Btech
+//
+//  Created by Student on 5/23/25.
+//
+
+import Foundation
+
+class EscapeModel {
+    @Published private(set) var secretCode: [Int] = [0,0,0,0]
+    
+    func generateSecretCode
+    
+}
