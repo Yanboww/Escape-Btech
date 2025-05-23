13678740be109fd86828c18e70a23fe7ee779d28
null <null>
2025-05-23 09:16:25 -0400

    dialogue

---
+++ Escape_Btech/BaseGame/Dialogue.swift
@@ -0,0 +1,40 @@
+//
+//  Dialogue.swift
+//  Escape_Btech
+//
+//  Created by Student on 5/22/25.
+//
+
+import SwiftUI
+
+struct Dialogue: View {
+    let window:CGSize
+    let name: String
+    let dialogueText: String
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(.black)
+                .opacity(0.8)
+                .frame(width: window.width, height: window.height*0.35)
+            Text(name)
+                .foregroundStyle(.cyan)
+                .fontWeight(.bold)
+                .font(.system(size: 25))
+                .offset(x:-window.width*0.45,y:-window.height*0.1)
+            Text(dialogueText)
+                .foregroundStyle(.white)
+                .offset(x:0,y:window.height*0.05)
+                .font(.system(size: 20))
+            Image(systemName: "chevron.down")
+                .foregroundStyle(.cyan)
+                .offset(x:window.width*0.45,y:window.height*0.15)
+                .font(.system(size: 20))
+        }
+        .offset(x:0,y:-window.height*0.25)
+    }
+}
+
+#Preview {
+    Dialogue(window:  CGSize(width: 852,height: 393),name:"Perry", dialogueText: "The meaning of tung tung sahur. 'Tung Tung Sahur' is a traditional custom unique to Indonesia during the month of Ramadan, in which 'Tung Tung' is a onomatopoeic word that imitates the sound of hitting drums")
+}
